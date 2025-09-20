07f9666980c5d682ff3b1df781baceb87e2e5ed0
chesadaklangthin <chesada.k@kkumail.com>
2025-09-20 23:25:30 +0700

    5

--- gitmodel3d/demo.gd
+++ gitmodel3d/demo.gd
@@ -3,7 +3,11 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$hunkapong/AnimationPlayer.play("Melee-Library/run")
+	$hunkapong/AnimationPlayer.play("MeleeLib/Die1")
+	$hunkapong2/AnimationPlayer.play("MeleeLib/LightStrafeLwalk")
+	$hunkapong3/AnimationPlayer.play("MeleeLib/LightRunning")
+	$hunkapong4/AnimationPlayer.play("mixamo_com")
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
